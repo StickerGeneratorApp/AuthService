92ebcc9070d20de4df622b2613e6ea8830802d2f
Mateusz Bałuch <xopyip@gmail.com>
2022-11-01 19:52:21 +0100

    loading NamingStrategy from annotation in case of custom implementation

--- src/main/java/pl/baluch/stickergenerator/auth/util/DatabaseIndexUtil.java
+++ src/main/java/pl/baluch/stickergenerator/auth/util/DatabaseIndexUtil.java
@@ -5,9 +5,11 @@ import com.mongodb.client.model.Indexes;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import io.micronaut.context.annotation.Value;
-import io.micronaut.data.model.naming.NamingStrategies;
+import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.model.naming.NamingStrategy;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import lombok.SneakyThrows;
 import reactor.core.publisher.Mono;
 
 @Singleton
@@ -17,9 +19,12 @@ public class DatabaseIndexUtil {
     @Value("${mongodb.database.name:}")
     private String databaseName;
 
+    @SneakyThrows
     public <T> MongoCollection<T> getCollection(Class<T> modelClass) {
+        NamingStrategy namingStrategy = modelClass.getAnnotation(MappedEntity.class)
+                .namingStrategy().getDeclaredConstructor().newInstance();
         return client.getDatabase(databaseName)
-                .getCollection(NamingStrategies.UnderScoreSeparatedLowerCase.DEFAULT.mappedName(modelClass.getSimpleName()), modelClass);
+                .getCollection(namingStrategy.mappedName(modelClass.getSimpleName()), modelClass);
     }
 
     public Mono<String> createUniqueIndex(Class<?> typeClass, String columnName) {
